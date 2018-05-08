6141077f17d8008b8e4e2e9570991ec84b80195b
evilGil <gilkahan@gmail.com>
2018-05-09 00:00:29 +0300

    import old project - inital stage

---
+++ src/app/header/cart-icon/cart-icon.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-cart-icon',
+  templateUrl: './cart-icon.component.html',
+  styleUrls: ['./cart-icon.component.css']
+})
+export class CartIconComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
